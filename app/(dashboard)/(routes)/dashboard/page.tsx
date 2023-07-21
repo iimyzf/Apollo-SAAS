257a09836a3a3e6bb6fca19ac4576adb526edcf1
iimyzf <agnaou004@gmail.com>
2023-07-21 17:24:42 +0100

    added the tools needed

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,90 @@
+"use client";
+
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import {
+    ArrowRight,
+    CodeIcon,
+    ImageIcon,
+    MessageSquare,
+    MusicIcon,
+    VideoIcon,
+} from "lucide-react";
+
+const tools = [
+    {
+        label: "Conversation",
+        icon: MessageSquare,
+        color: "text-violet-500",
+        bgColor: "bg-violet-500/10",
+        href: "/conversation",
+    },
+    {
+        label: "Image Generation",
+        icon: ImageIcon,
+        color: "text-pink-500",
+        bgColor: "bg-pink-500/10",
+        href: "/image",
+    },
+    {
+        label: "Video Generation",
+        icon: VideoIcon,
+        color: "text-orange-500",
+        bgColor: "bg-orange-500/10",
+        href: "/video",
+    },
+    {
+        label: "Music Generation",
+        icon: MusicIcon,
+        color: "text-blue-500",
+        bgColor: "bg-blue-500/10",
+        href: "/music",
+    },
+    {
+        label: "Code Generation",
+        icon: CodeIcon,
+        color: "text-green-500",
+        bgColor: "bg-green-500/10",
+        href: "/code",
+    },
+];
+
 const DashboardPage = () => {
-    return <div></div>;
+    return (
+        <div>
+            <div className="mb-8 space-y-4">
+                <h2 className="text-2xl md:text-4xl text-center font-bold">
+                    Explore the power of AI
+                </h2>
+                <p className="text-muted-foreground text-center font-light text-sm md:text-2xl">
+                    Enhance your experience with Apollo AI
+                </p>
+            </div>
+            <div className="px-4 md:px-20 lg:px-32 space-y-4">
+                {tools.map((tool) => (
+                    <Card
+                        key={tool.href}
+                        className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+                    >
+                        <div className="flex items-center gap-x-4">
+                            <div
+                                className={cn(
+                                    "p-2 w-fit rounded-md",
+                                    tool.bgColor
+                                )}
+                            >
+                                <tool.icon
+                                    className={cn("w-8 h-8", tool.color)}
+                                />
+                            </div>
+                            <div className="font-semibold">{tool.label}</div>
+                        </div>
+                        <ArrowRight className="w-5 h-5" />
+                    </Card>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default DashboardPage;
