26c87563aa4588c38c8999fb9d24c1617cd60f45
iimyzf <agnaou004@gmail.com>
2023-07-23 17:19:08 +0100

    video gen is done

---
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import * as z from "zod";
+import { VideoIcon } from "lucide-react";
+import Heading from "@/components/heading";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { formSchema } from "./constants";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import axios from "axios";
+import Empty from "@/components/empty";
+import Loader from "@/components/loader";
+
+const VideoPage = () => {
+    const router = useRouter();
+    const [video, setVideo] = useState<string>();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            prompt: "",
+        },
+    });
+
+    const isLoading = form.formState.isSubmitting;
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            setVideo(undefined);
+
+            const response = await axios.post("/api/video", values);
+
+            setVideo(response.data[0]);
+
+            form.reset();
+        } catch (error: any) {
+            console.log(error);
+        } finally {
+            router.refresh();
+        }
+    };
+
+    return (
+        <div>
+            <Heading
+                title="Video Generation"
+                description="Generate text from a prompt"
+                icon={VideoIcon}
+                iconColor="text-orange-500"
+                bgColor="bg-orange-500/10"
+            />
+            <div className="px-4 lg:px-8">
+                <div>
+                    <Form {...form}>
+                        <form
+                            onSubmit={form.handleSubmit(onSubmit)}
+                            className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+                        >
+                            <FormField
+                                name="prompt"
+                                render={({ field }) => (
+                                    <FormItem className="col-span-12 lg:col-span-10">
+                                        <FormControl className="p-0 m-0">
+                                            <Input
+                                                className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                                                disabled={isLoading}
+                                                placeholder="Enter a prompt"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                            <Button
+                                className=" col-span-12 lg:col-span-2 w-full"
+                                disabled={isLoading}
+                            >
+                                Generate
+                            </Button>
+                        </form>
+                    </Form>
+                </div>
+                <div className="space-y-4 mt-4">
+                    {isLoading && (
+                        <div className="rounded-lg p-8 w-full flex items-center justify-center bg-muted">
+                            <Loader />
+                        </div>
+                    )}
+                    {!video && !isLoading && (
+                        <Empty label="No conversation here!" />
+                    )}
+                    {video && (
+                        <video
+                            className="w-full aspect-video mt-8 rounded-lg border bg-black"
+                            controls
+                        >
+                            <source src={video} />
+                        </video>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default VideoPage;
