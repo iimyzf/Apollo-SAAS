dec97980db01ad30b7318e0a3df9597e16db5054
iimyzf <agnaou004@gmail.com>
2023-07-24 14:57:32 +0100

    added the api limit for the user in all the gens

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -1,6 +1,7 @@
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import { Configuration, OpenAIApi, ChatCompletionRequestMessage } from "openai";
+import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
@@ -30,10 +31,20 @@ export async function POST(req: Request) {
         if (!messages)
             return new NextResponse("Messages not found", { status: 400 });
 
+        const freeTrial = await checkApiLimit();
+
+        if (!freeTrial) {
+            return new NextResponse("Free trial limit reached", {
+                status: 403,
+            });
+        }
+
         const response = await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
             messages: [instructionMessage, ...messages],
         });
+
+        await increaseApiLimit();
 
         return NextResponse.json(response.data.choices[0].message);
     } catch (error) {
