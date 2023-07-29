1e1dc2dffe3cf2b914e9d2aebc2b6ac1bf8daa49
iimyzf <agnaou004@gmail.com>
2023-07-29 15:55:21 +0100

    added a cute little toast for ux again

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -24,6 +24,7 @@ import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
 import axios from "axios";
 import { useState } from "react";
+import { toast } from "react-hot-toast";
 
 const tools = [
     {
@@ -69,7 +70,7 @@ const ProModal = () => {
 
             window.location.href = response.data.url;
         } catch (error) {
-            console.log("[CLIENT_ERROR]", error);
+			toast.error("Something went wrong!");
         } finally {
 			setLoading(false);
 		}
